72227b28b004425df901d6251309d7b1f0a8e2de
Tejas Mahamuni <tejasmahamuni16@gmail.com>
2026-08-11 08:39:53 +0530

    Time: 29 ms (6.68%), Space: 107 MB (54.81%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if (map.get(nums[i]) >= 2) {
+                return true;
+            }
+        }
+        
+        // for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+        //     if (e.getValue() >= 2) {
+        //         return true;
+        //     }
+        // }
+        return false;
+    }
+}
