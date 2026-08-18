4d90b6bb5f995dc4626fbf68776a915bae1eb291
Tejas Mahamuni <tejasmahamuni16@gmail.com>
2026-08-18 14:24:36 +0530

    Time: 1 ms (77.4%), Space: 46.6 MB (64.16%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,10 +3,7 @@ class Solution {
         int j = 0;
 
         for (int i=1; i<nums.length; i++) {
-            if (nums[i] == nums[j]) {
-                continue;
-            }
-            else {
+            if (nums[i] != nums[j]) {
                 nums[j+1] = nums[i];
                 j++;
             }
