20eadf24be22ef1c73e092ee48ed6d8913f51a30
Tejas Mahamuni <tejasmahamuni16@gmail.com>
2026-09-14 11:02:17 +0530

    Time: 532 ms (5.24%), Space: 47.2 MB (10.82%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,38 +1,24 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> result = new HashSet<>();
 
-        Arrays.sort(nums);
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                Set<Long> map = new HashSet<>();
+                for (int k=j+1; k<nums.length; k++) {
+                    long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    long req = (long)target - sum;
 
-        for (int i=0; i<nums.length-3; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-
-            for (int j=i+1; j<nums.length-2; j++) {
-                if (j > i+1 && nums[j] == nums[j-1]) continue;
-
-                int left = j+1;
-                int right = nums.length -1;
-
-                while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-                    if (sum == target) {
-                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-
-                        left++;
-                        right--;
-
-                        while (left < right && nums[left] == nums[left-1]) left++;
-                        while (left < right && nums[right] == nums[right+1]) right--;
-                    } 
-                    else if (sum < target) {
-                        left++;
+                    if (map.contains(req)) {
+                        List<Integer> row = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], (int) req));
+                        Collections.sort(row);
+                        result.add(row);
                     }
-                    else if (sum > target) {
-                        right--;
-                    }
+                    map.add((long)nums[k]);
                 }
             }
         }
-        return res;
+        return new ArrayList<>(result);
     }
 }
